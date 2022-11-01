8831a145e2fcd2ccaff0730a5bae081e954b679c
harndt <harndt@student.42sp.org.br>
2022-11-01 01:27:59 +0100

    Add debug

---
+++ debug/debug.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbonaldi <bbonaldi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 19:09:40 by bbonaldi          #+#    #+#             */
+/*   Updated: 2022/10/28 19:26:24 by bbonaldi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_print_tokens_list(t_token *token_head)
+{
+	t_token		*head;
+	static char	*types[] = {"NOT_FOUND", "WORD", "IO_FILE_TRUNCATE",
+		"IO_FILE_APPEND", "IO_FILE_INFILE", "IO_HEREDOC", "OR_IF", "AND_IF",
+		"VAR_EXPRESSION", "WILDCARD", "PIPELINE", "OPEN_PARENTHESIS",
+		"CLOSE_PARENTHESIS", "SINGLE_QUOTE", "DOUBLE_QUOTE"};
+
+	head = token_head;
+	while (head)
+	{
+		ft_printf(GREEN"----------------------------\n"RESET);
+		ft_printf(YELLOW"token: %s\n"RESET, head->token);
+		ft_printf(BLUE"type: %s\n"RESET, types[head->type]);
+		ft_printf(GREEN"----------------------------\n"RESET);
+		head = head->next;
+	}
+}
